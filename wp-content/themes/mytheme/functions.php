e9a897568f20a61172c83553ac78cbf6150f8581
brijmohan-cedcoss <brijmohanpandey@cedcoss.com>
2020-06-01 20:21:02 +0530

    third change

--- wp-content/themes/mytheme/functions.php
+++ wp-content/themes/mytheme/functions.php
@@ -50,7 +50,12 @@ add_action( 'init', 'register_my_menus' );
 /**
  * Support thumbnails.
  */
-add_theme_support( 'post-thumbnails' );
+if ( function_exists( 'add_theme_support' ) ) {
+	add_theme_support( 'post-thumbnails' );
+	set_post_thumbnail_size( 150, 150, true ); // default Post Thumbnail dimensions (cropped).
 
-
+	// Additional image sizes
+	// Delete the next line if you do not need additional image sizes.
+	add_image_size( 'category-thumb', 300, 9999 ); // 300 pixels wide (and unlimited height)
+}
 
