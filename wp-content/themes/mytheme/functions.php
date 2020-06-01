bef39b803c861546abd8d85f16f423be9da0d48e
brijmohan-cedcoss <brijmohanpandey@cedcoss.com>
2020-06-01 22:12:04 +0530

    fifth change

--- wp-content/themes/mytheme/functions.php
+++ wp-content/themes/mytheme/functions.php
@@ -80,4 +80,83 @@ add_action( 'after_setup_theme', 'themename_custom_header_setup' );
  */
 add_theme_support( 'custom-background' );
 
+/**
+ * Post formats.
+ */
+function themename_post_formats_setup() {
+	add_theme_support( 'post-formats', array( 'aside', 'gallery' ) );
+}
+add_action( 'after_setup_theme', 'themename_post_formats_setup' );
+
+/**
+ * Sidebar support.
+ */
+function my_register_sidebars() {
+	/* Register the 'primary' sidebar. */
+	register_sidebar(
+		array(
+			'id'            => 'primary',
+			'name'          => __( 'Primary Sidebar' ),
+			'description'   => __( 'A short description of the sidebar.' ),
+			'before_widget' => '<div id="%1$s" class="widget %2$s">',
+			'after_widget'  => '</div>',
+			'before_title'  => '<h3 class="widget-title">',
+			'after_title'   => '</h3>',
+		)
+	);
+	/* Repeat register_sidebar() code for additional sidebars. */
+}
+add_action( 'widgets_init', 'my_register_sidebars' );
+
+/**
+ * Custom logo support.
+ */
+function themename_custom_logo_setup() {
+	$defaults = array(
+		'height'      => 100,
+		'width'       => 400,
+		'flex-height' => true,
+		'flex-width'  => true,
+		'header-text' => array( 'site-title', 'site-description' ),
+	);
+	add_theme_support( 'custom-logo', $defaults );
+}
+add_action( 'after_setup_theme', 'themename_custom_logo_setup' );
+
+/**
+ * Title tag support.
+ */
+add_theme_support( 'title-tag' );
+
+/**
+ * Automaic feed link support.
+ */
+add_theme_support( 'automatic-feed-links' );
+
+/**
+ * Html5 support
+ */
+add_theme_support( 'html5', array( 'comment-list', 'comment-form', 'search-form', 'gallery', 'caption', 'style', 'script' ) );
+
+/**
+ * Editor style support
+ */
+add_theme_support( 'editor-styles' );
+
+
+if ( ! isset( $content_width ) ) {
+	$content_width = 640;
+}
+/**
+ * Content Width
+ */
+function mytheme_adjust_content_width() {
+	global $content_width;
+
+	if ( is_page_template( 'full-width.php' ) ) {
+		$content_width = 780;
+	}
+}
+add_action( 'template_redirect', 'mytheme_adjust_content_width' );
+
 
