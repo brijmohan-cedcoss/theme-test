92af5af25fe8473f27b76fbe1371147c5036a7b1
brijmohan-cedcoss <brijmohanpandey@cedcoss.com>
2020-06-01 20:43:27 +0530

    fourth change

--- wp-content/themes/mytheme/functions.php
+++ wp-content/themes/mytheme/functions.php
@@ -59,3 +59,25 @@ if ( function_exists( 'add_theme_support' ) ) {
 	add_image_size( 'category-thumb', 300, 9999 ); // 300 pixels wide (and unlimited height)
 }
 
+/**
+ * Custom header.
+ */
+function themename_custom_header_setup() {
+	$args = array(
+		'default-image'      => get_template_directory_uri() . 'img/default-image.jpg',
+		'default-text-color' => '000',
+		'width'              => 1000,
+		'height'             => 250,
+		'flex-width'         => true,
+		'flex-height'        => true,
+	);
+	add_theme_support( 'custom-header', $args );
+}
+add_action( 'after_setup_theme', 'themename_custom_header_setup' );
+
+/**
+ * Custom Background.
+ */
+add_theme_support( 'custom-background' );
+
+
